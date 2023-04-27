d423b08ba76050e1e15f8e57240ac231ef584b53
imparcialista <lucasaraujoz13@outlook.com>
2023-04-26 21:14:50 -0300

    backup

---
+++ code/primeiro - Copia/script.js
@@ -0,0 +1,54 @@
+const squares = document.querySelectorAll('.square');
+let player = 'X';
+let gameOver = false;
+
+function handleMove(square) {
+  if (gameOver == true) {
+    alert(`Player ${player} wins!`);
+  }
+
+  if (!gameOver && square.innerText === '') {
+    
+    square.innerText = player;
+    checkForWin();
+    player = player === 'X' ? 'O' : 'X';
+    
+  }  
+  
+
+}
+
+function checkForWin() {
+  const winningConditions = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+  for (let i = 0; i < winningConditions.length; i++) {
+    const [a, b, c] = winningConditions[i];
+    if (
+      squares[a].innerText &&
+      squares[a].innerText === squares[b].innerText &&
+      squares[a].innerText === squares[c].innerText
+    ) {
+      gameOver = true;
+      return;
+    }
+  } 
+} 
+
+squares.forEach((square) => {
+  
+  square.addEventListener('click', () => {
+    checkForWin();
+    handleMove(square);
+
+  } ) ;
+
+} ) ; 
+
